94da2ead6c2d89dba2bb6324db2e3846f8dc157b
Prem <prem7736k@gmail.com>
2025-05-28 11:01:39 +0530

    Fix import path case sensitivity for Vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
 import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
-import Home from './components/pages/Home/home';
-import Cart from './components/pages/Cart/cart';
-import PlaceOrder from './components/pages/PlaceOrder/placeOrder';
+import Home from './components/pages/Home/Home';
+import Cart from './components/pages/Cart/Cart';
+import PlaceOrder from './components/pages/PlaceOrder/PlaceOrder';
 import Fotter from './components/Fotter/Fotter';
 import LoginPopup from './components/LoginPopup/LoginPopup';
 
-
 const App = () => {
-  const [showLogin,setShowLogin] = useState(false)
-  
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
-  <>
-  {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
+    <>
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : null}
       <div className="app">
-      <Navbar setShowLogin={setShowLogin} />
-      <Routes>
-       <Route path='/' element={<Home/>}/>
-       <Route path='/cart' element={<Cart/>}/>
-       <Route path='/order' element={<PlaceOrder/>}/>
-       
-      </Routes>
-    </div>
-    <Fotter/>
-  
-  </>
+        <Navbar setShowLogin={setShowLogin} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Fotter />
+    </>
   );
 };
 
